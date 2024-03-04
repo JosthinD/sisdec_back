6b1cc96fb55bc51d1662647cb96b39b3449f69eb
juanrodriguezr <juanrodriguezr@periferia-it.com>
2024-03-04 00:16:22 -0500

    ll

--- Aplicacion/obj/Debug/net6.0/Aplicacion.DTO.AssemblyInfo.cs
+++ Aplicacion/obj/Debug/net6.0/Aplicacion.DTO.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacion.DTO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f87bf05cbc42a3444ff6290e4fbd3e2366a2b16")]
->>>>>>> Camilo
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacion.DTO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacion.DTO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
