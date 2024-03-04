7872ba81863bf7e3e57cac323ac3a596ca609d01
DESKTOP-62H9VHB\Josthin <cadetejd15@gmail.com>
2024-03-04 18:42:50 -0500

    copia JDSS

--- Repositorio.Repositorio/obj/Debug/net6.0/Repositorio.Repositorio.AssemblyInfo.cs
+++ Repositorio.Repositorio/obj/Debug/net6.0/Repositorio.Repositorio.AssemblyInfo.cs
@@ -15,12 +15,6 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-<<<<<<< HEAD
-=======
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+116f37eb5ec1c1c3db17190ae989ca7babdbdc90")]
->>>>>>> Camilo
->>>>>>> 3b6b9cd9c70e15d7965b2c358126319f75f80705
 [assembly: System.Reflection.AssemblyProductAttribute("Repositorio.Repositorio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositorio.Repositorio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
